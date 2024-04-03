28af9b94c26344c5b229bbb77b4148a418915974
kuruvapavani <pavanikuruva2109@gmail.com>
2024-04-03 10:30:39 +0530

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import NotificationBar from "./pages/components/NotificationBar";
-
+import SideBar from "./pages/components/SideBar"
 
 function App() {
   return (
     <div>
    <NotificationBar />
+   <div style={{ marginTop: "250px" }}> {/* Adjust margin-left based on the width of your sidebar */}
+        <SideBar />
+      </div>
     </div>
   );
 }
--- src/pages/components/NotificationBar.jsx
+++ src/pages/components/NotificationBar.jsx
@@ -13,7 +13,7 @@ const NotificationBar = () => {
   return (
     <div>
       {notificationVisible && (
-        <div className="bg-blue-600 p-3 text-white text-center flex items-center justify-center notification">
+        <div className="bg-blue-600 p-3 text-white text-center flex items-center justify-center notification fixed top-0 left-0 right-0 z-50">
           <div style={{ whiteSpace: "nowrap" }}>Enable browser notifications to avoid missing out on important activities.</div>
           <div className='flex text-center'>
             <button className='bg-blue-700 hover:bg-blue-800 p-1 pl-2 pr-2 rounded mr-3' style={{ whiteSpace: "nowrap", fontSize: "0.6rem" }}>
