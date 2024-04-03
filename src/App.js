629a00d18f4c87b4b37617bc7e64a7a1c99fd8af
kuruvapavani <pavanikuruva2109@gmail.com>
2024-04-03 15:47:45 +0530

    remove unused components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import NotificationBar from "./pages/components/NotificationBar";
-import SideBar from "./pages/components/SideBar"
 import Home from "./pages/Home";
-import About from "./pages/About";
 function App() {
   return (
     <div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import SideBar from './components/SideBar';
-import NotificationBar from './components/NotificationBar';
 const About = () => {
   return (
     <div>
